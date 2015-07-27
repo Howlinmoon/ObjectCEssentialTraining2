562c8240ca269f74d074b95fca6e5d5fbb87545e
Jim Veneskey <github@strifecrafter.com>
2015-07-27 12:41:42 -0400

    Starting the last project - the debugging demo

---
+++ DebuggingDemo/DebuggingDemo/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  DebuggingDemo
+//
+//  Created by jim Veneskey on 7/27/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
